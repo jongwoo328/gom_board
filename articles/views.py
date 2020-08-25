72cbb93fca1f1864fccfc77b53d2889804030c4f
HwnagYoungJun <dudghkd4512@gmail.com>
2020-08-25 22:55:20 +0900

    almost

--- articles/views.py
+++ articles/views.py
@@ -6,8 +6,8 @@ from rest_framework.response import Response
 
 from drf_yasg.utils import swagger_auto_schema
 
+from .models import Article, Comment
 from .serializers import ArticleSerializer, CommentSerializer
-
 
 class ArticleView(APIView):
 
@@ -15,11 +15,6 @@ class ArticleView(APIView):
   def post(self, request):
     """
         # Article을 작성하는 요청
-        ---
-        ## 내용
-            - ssss: zzz
-            - zzzz : ddd
-     
     """
     serializer = ArticleSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
@@ -42,6 +37,7 @@ class ArticleDetailView(APIView):
     serializer = ArticleSerializer(article)
     return Response(serializer.data, status=status.HTTP_200_OK)
   
+  @swagger_auto_schema(request_body=ArticleSerializer)
   def patch(self, request, article_pk):
     article = self.get_object(article_pk)
     serializer = ArticleSerializer(instance=article, data=request.data)
@@ -52,14 +48,14 @@ class ArticleDetailView(APIView):
 
 class CommentView(APIView):
   
-  def get(self, article_pk):
+  def get(self, request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
-    comments = article.comment.objects.all()
-    print(comments)
+    comments = article.comment_set.all()
     serializer = CommentSerializer(comments, many=True)
-    return Response(serializer, status=status.HTTP_200_OK)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
-  def post(self, article_pk):
+  @swagger_auto_schema(request_body=CommentSerializer)
+  def post(self, request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
     serializer = CommentSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
@@ -72,15 +68,16 @@ class CommentDetailView(APIView):
   def get_object(self, comment_pk):
     return get_object_or_404(Comment, pk=comment_pk)
   
-  # don't use this function
-  def patch(self, request, comment_pk):
+  # don't use this function.
+  @swagger_auto_schema(request_body=CommentSerializer)
+  def patch(self, request, article_pk, comment_pk):
     comment = self.get_object(comment_pk)
     serializer = CommentSerializer(instance=comment, data=request.data)
     if serializer.is_valid(raise_exception=True):
       serializer.save()
       return Response(serializer.data, status=status.HTTP_200_OK)
     
-  def delete(self, request, comment_pk):
+  def delete(self, request, article_pk, comment_pk):
     comment = self.get_object(comment_pk)
     comment.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
