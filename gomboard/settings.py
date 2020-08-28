625f5b2d646e3eeef1679472a19843eb924e67c4
JongWoo <jongwoo328@gmail.com>
2020-08-28 19:52:04 +0900

    cors setting

--- gomboard/settings.py
+++ gomboard/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
 
+    'corsheaders',
+
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -96,12 +98,22 @@ SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware'
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://jongwoo328.pythonanywhere.com",
+    "http://localhost:8080",
+    "http://localhost:8081"
+]
+
 
 ROOT_URLCONF = 'gomboard.urls'
 
