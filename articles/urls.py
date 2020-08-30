daa4ccc6b0e4ea8e6f414bfd2a1fe0296b69910c
HwnagYoungJun <dudghkd4512@gmail.com>
2020-08-30 18:06:54 +0900

    articles

--- articles/urls.py
+++ articles/urls.py
@@ -6,6 +6,7 @@ app_name = 'articles'
 urlpatterns = [
   path('', views.ArticleView.as_view()),
   path('<int:article_pk>/', views.ArticleDetailView.as_view(), name='article_detail'),
+  path('<int:article_pk>/bookmark/', views.ArticleBookmarkView.as_view(), name="article_bookmark"),
   path('<int:article_pk>/comment/', views.CommentView.as_view(), name='comment'),
   path('<int:article_pk>/comment/<int:comment_pk>/', views.CommentDetailView.as_view(), name='comment_detail'),
 ]
