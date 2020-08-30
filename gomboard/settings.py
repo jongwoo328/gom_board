f361d9b999a984b10339b512c81f814b1fbf9656
JongWoo <jongwoo328@gmail.com>
2020-08-30 17:34:57 +0900

    modify jwt expiration delta

--- gomboard/settings.py
+++ gomboard/settings.py
@@ -97,7 +97,7 @@ REST_FRAMEWORK = {
 
 JWT_AUTH = {
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=1800)
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7)
 }
 
 SITE_ID = 1
